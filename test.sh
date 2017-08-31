c3306dcd670de54f88a1476df692d45d7cac5a9e
mandaman2k <ilanado@gmail.com>
2017-08-31 15:10:25 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,9 +3,8 @@ apt-get update && sudo apt-get install -y libmicrohttpd-dev libssl-dev cmake bui
 git clone https://github.com/mandaman2k/xmr-stak-cpu.git
 cd xmr-stak-cpu/
 cmake .
-make
+make install
 cd bin
-wget http://104.131.78.117:4032/azl01/config.txt
 echo "* soft memlock 262144" >> /etc/security/limits.conf
 echo "* hard memlock 262144" >> /etc/security/limits.conf
 sysctl -w vm.nr_hugepages=128
